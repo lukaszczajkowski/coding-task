fc07c84ddc46378de89f3ab51d8acd4ae8338524
Lukasz  Czajkowski <lukaszczajkowski@sda-11-mbpro.lan>
2020-11-22 20:17:02 +0100

    Tasks 1 and 3 implemented, basic tests included.

--- src/main/java/com/minnity/report/ReportService.java
+++ src/main/java/com/minnity/report/ReportService.java
@@ -1,24 +1,92 @@
 package com.minnity.report;
 
-import org.apache.commons.lang3.NotImplementedException;
-
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ReportService {
 
   //task 1: Return number of requests that were made for each company. (e.g. companyId -> requestNumber)
   public Map<Integer, Long> calculateNumberOfRequestsPerCompany(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
+    Map<Integer, Long> requestsPerCompany = new HashMap<>();
+
+    requestLogs.stream().map(requestLog -> requestLog.getCompanyId())
+                        .forEach(request -> {
+      if(requestsPerCompany.containsKey(request)){
+        Long numOfRequests = requestsPerCompany.get(request) + 1;
+        requestsPerCompany.replace(request, numOfRequests);
+      } else {
+        requestsPerCompany.put(request, 1L);
+      }
+    });
+
+    return requestsPerCompany;
   }
 
   //task 2: Count and return requests per company that finished with an error HTTP response code (>=400)
   public Map<Integer, RequestLog> findRequestsWithError(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
+    Map<Integer, RequestLog> requestsWithError = new HashMap();
+    requestLogs.stream()
+                .filter(requestLog -> requestLog.getRequestStatus()>=400)
+                .forEach(requestLog -> {
+                  Integer companyId = requestLog.getCompanyId();
+                  if(requestsWithError.containsKey(companyId)){
+
+                  }
+                });
+    return requestsWithError;
   }
 
   //task 3: find and print API (requests path) that on average takes the longest time to process the request.
   public String findRequestPathWithLongestDurationTime(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
+      Map<String, List<Long>> processTimesPerRequest = getProcessTimesPerRequest(requestLogs);
+      Map<String, Long> averageTimesPerRequest = getAverageTimesPerRequest(processTimesPerRequest);
+      return getRequestPathWithLongestDurationTime(averageTimesPerRequest);
   }
+
+  private Map<String, List<Long>> getProcessTimesPerRequest(List<RequestLog> requestLogs) {
+      Map<String, List<Long>> processTimesPerRequest = new HashMap<>();
+
+      requestLogs.forEach(requestLog -> {
+          String requestPath = requestLog.getRequestPath();
+          long processTime = requestLog.getRequestDuration();
+          if(!processTimesPerRequest.containsKey(requestPath)){
+              List<Long> listOfProcessTimes = new ArrayList<>();
+              listOfProcessTimes.add(processTime);
+              processTimesPerRequest.put(requestPath, listOfProcessTimes);
+          } else {
+              processTimesPerRequest.get(requestPath).add(processTime);
+          }
+      });
+
+      return processTimesPerRequest;
+  }
+
+  private Map<String, Long> getAverageTimesPerRequest(Map<String, List<Long>> processTimesPerRequest) {
+      Map<String, Long> averageTimesPerRequest = new HashMap<>();
+      processTimesPerRequest.forEach((requestPath, requestDurationList) -> {
+          long requestDurationSum =
+                  requestDurationList.stream()
+                                    .reduce(0L, (current, count) -> current + count);
+          long averageTime = requestDurationSum/requestDurationList.size();
+          averageTimesPerRequest.put(requestPath, averageTime);
+      });
+
+      return averageTimesPerRequest;
+  }
+
+  private String getRequestPathWithLongestDurationTime(Map<String, Long> averageTimesPerRequest) {
+      long maxAverage = Long.MIN_VALUE;
+      String requestPathWithLongestDurationTime = null;
+
+      for (Map.Entry entry: averageTimesPerRequest.entrySet()) {
+          String key = (String) entry.getKey();
+          long currentAverage = (long) entry.getValue();
+          if(currentAverage > maxAverage) {
+              maxAverage = currentAverage;
+              requestPathWithLongestDurationTime = key;
+          }
+      }
+      return requestPathWithLongestDurationTime;
+  }
+
 }
--- src/test/java/com/minnity/report/ReportServiceTest.java
+++ src/test/java/com/minnity/report/ReportServiceTest.java
@@ -2,11 +2,101 @@ package com.minnity.report;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
 
 public class ReportServiceTest {
 
+  ReportService reportService;
+
   @Test
-  public void todo() {
-    RequestLog sampleRequest = SampleDataGenerator.aRequestLog();
+  public void calculateNumberOfRequestsPerCompanyTest() {
+      reportService = new ReportService();
+      List<RequestLog> requestLogs = new ArrayList<>();
+
+      //Case: the requestLogs list is empty
+
+      Map<Integer, Long> expectedMap = new HashMap<>();
+      Map<Integer, Long> actualMap =
+              reportService.calculateNumberOfRequestsPerCompany(requestLogs);
+
+      assertEquals(expectedMap, actualMap);
+      assertEquals(expectedMap.size(), actualMap.size());
+
+      //Case: the requestLogs list contains 3 different company ids
+
+      for (int i = 0; i < 3; i++) {
+        RequestLog requestLog = RequestLog.RequestLogBuilder
+                .aRequestLog()
+                .withCompanyId(i)
+                .build();
+        requestLogs.add(requestLog);
+      }
+      actualMap = reportService.calculateNumberOfRequestsPerCompany(requestLogs);
+      expectedMap = new HashMap<>();
+
+      for(RequestLog log : requestLogs) {
+        expectedMap.put(log.getCompanyId(), 1L);
+      }
+
+      assertEquals(expectedMap, actualMap);
+      assertEquals(expectedMap.size(), actualMap.size());
+
+      //Case: the requestLog list contains 4 company ids, 1 is duplicated
+
+      RequestLog requestLog1 = RequestLog.RequestLogBuilder
+              .aRequestLog()
+              .withCompanyId(2)
+              .build();
+      requestLogs.add(requestLog1);
+      expectedMap.put(2, 2L);
+      actualMap = reportService.calculateNumberOfRequestsPerCompany(requestLogs);
+
+      assertEquals(expectedMap, actualMap);
+      assertEquals(expectedMap.size(), actualMap.size());
   }
+
+  @Test
+  public void findRequestPathWithLongestDurationTimeTest() {
+      reportService = new ReportService();
+      List<RequestLog> requestLogs = prepareRequestLogList(9);
+      String actual = reportService.findRequestPathWithLongestDurationTime(requestLogs);
+      String expected = "/vivaldi";
+
+      assertEquals(expected, actual);
+  }
+
+
+  private RequestLog prepareRequestLog(String endpoint, long requestDuration) {
+    return RequestLog.RequestLogBuilder.aRequestLog()
+            .withRequestPath(endpoint)
+            .withRequestDuration(requestDuration)
+            .build();
+  }
+
+  private List<RequestLog> prepareRequestLogList(int numOfLogs) {
+      List<RequestLog> requestLogs = new ArrayList<>();
+      RequestLog requestLog = null;
+      String endpoint = null;
+
+      for(int i = 0; i < numOfLogs; i++) {
+          long requestDuration = i * 100;
+          if(i < 3) {
+              endpoint = "/ping";
+          } else if (i >= 3 && i < 6) {
+              endpoint = "/users";
+          } else {
+              endpoint = "/vivaldi";
+          }
+          requestLog = prepareRequestLog(endpoint, requestDuration);
+          requestLogs.add(requestLog);
+      }
+
+      return requestLogs;
+  }
+
 }
